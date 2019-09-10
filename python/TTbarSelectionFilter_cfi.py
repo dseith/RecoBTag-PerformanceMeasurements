20170edf141f66c751c000fefe85416d566cc44f
David Seith <david.seith@cern.ch>
2019-09-10 10:20:25 +0200

    mtd changes from Daniel

--- python/TTbarSelectionFilter_cfi.py
+++
@@ -1,7 +0,0 @@
-import FWCore.ParameterSet.Config as cms
-
-ttbarselectionfilter = cms.EDFilter('TTbarSelectionFilter',
-                                    selectChannels = cms.vint32(-11*11,-13*13,-11*13), #dilepton channels
-				    #selectChannels = cms.vint32(-11,11,-13,13), #semileptonic channels
-                                    selectAll      = cms.bool(False)
-                                    )
--- python/defaults/PhaseII.py
+++ python/defaults/PhaseII.py
@@ -5,11 +5,17 @@ common = {
 	'usePuppi' : True,
 	'usePuppiForFatJets' : True,
 	'usePuppiForBTagging' : False,
-	'mcGlobalTag' : '103X_upgrade2023_realistic_v2',
+	'mcGlobalTag' : '106X_upgrade2023_realistic_v3',
 	'remakeAllDiscr' : True,
 	'maxJetEta' : 4.5,
 	'usePrivateJEC' : True,
 	'jecDBFileMC' : 'PhaseIIFall17_V3_MC', 
-#	'inputFiles' : ['/store/relval/CMSSW_10_4_0_mtd3/RelValTTbar_Tauola_14TeV/MINIAODSIM/PU25ns_103X_upgrade2023_realistic_v2_2023D35PU200_1-v2/20000/31C1C942-EC8D-1245-B773-2293F5CC87DB.root',],
+#	'inputFiles' : ['file:gensimreco_0089F52F-2F32-BB4B-87FF-DDE7BC7DD9F2.root'],
+#	'inputFiles' : ['file:miniaodsim_A27FB6CA-7D30-1C46-A8BF-E79A54D2B7D8.root'],
+#	'inputFiles' : ['file:miniaodsim_ttbar_0A87FDFA-3ACC-BE42-A051-ACD5E7882B73.root'],
+#	'inputFiles' : ['file:miniaodsim_RelValTTbar_Tauola_14TeV_CMSSW_10_4_0_mtd5-103X_upgrade2023_realistic_v2_2023D35noPU-v1.root'],
+#	'inputFiles' : ['/store/relval/CMSSW_10_4_0_mtd5/RelValTTbar_Tauola_14TeV/MINIAODSIM/PU25ns_103X_upgrade2023_realistic_v2_2023D35PU200-v1/20000/A27FB6CA-7D30-1C46-A8BF-E79A54D2B7D8.root']
+#	'inputFiles' : ['/store/relval/CMSSW_10_4_0_mtd5/RelValTTbar_Tauola_14TeV/MINIAODSIM/PU25ns_103X_upgrade2023_realistic_v2_2023D35PU200-v1/20000/C4F8225C-BC78-B749-B465-07FE610DA454.root']
+#	'inputFiles' : ['/store/relval/CMSSW_10_4_0_mtd5/RelValTTbar_Tauola_14TeV/GEN-SIM-RECO/PU25ns_103X_upgrade2023_realistic_v2_2023D35PU200-v1/20000/FDADD684-84EE-2442-867F-6DB15017F926.root'],
 	'JPCalibration' : 'JPcalib_MC81X_v0',
 }
